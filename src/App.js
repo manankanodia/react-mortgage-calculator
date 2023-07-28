bcf0e7e986e69f8ec54f0a659976ad52e31b173f
manankanodia <manankanodia@gmail.com>
2023-07-28 16:32:45 +0530

    Setup folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-// import logo from './logo.svg';
-// import './App.css';
+import Navbar from "./components/Navbar";
+import SliderSelect from "./components/SliderSelect";
+import TenureSelect from "./components/TenureSelect";
+import Result from "./components/Result";
 
 function App() {
-  return (
-    <div className="App">
-      Hello React
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar/>
+            <SliderSelect/>
+            <TenureSelect/>
+            <Result/>
+        </div>
+    );
 }
 
 export default App;
