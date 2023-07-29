eeaad895ace41e5e612d7aa077000cf874395e53
manankanodia <manankanodia@gmail.com>
2023-07-29 15:14:54 +0530

    Added Navbar code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
 import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import {Container} from "@mui/material";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <AppBar position="static">
+            <Container maxWidth={'xl'}>
+                <Toolbar>
+                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                        Mortgage Calculator
+                    </Typography>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
