79fcea89dc28b6ddda9f34c86ffd9f0b8c596cc8
manankanodia <manankanodia@gmail.com>
2023-07-31 22:05:38 +0530

    Added the Result Pie Chart

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ function App() {
         <div className="App">
             <Navbar/>
             <Container maxWidth={'xl'} sx={{marginTop: 4}}>
-                <Grid container spacing={5}>
+                <Grid container spacing={5} alignItems='center'>
                     <Grid item xs={12} md={6}>
                         <SliderSelect data={data} setData={setData}/>
                         <TenureSelect data={data} setData={setData}/>
 
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <Result/>
+                        <Result data={data}/>
                     </Grid>
                 </Grid>
             </Container>
